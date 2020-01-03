2c8518995dd5b8a4f94cd346c63b3f5d53b985c0
DevMai90 <dmai90@yahoo.com>
2020-01-02 22:18:15 -0800

    Added click actions within the component's HBS file and defined action helpers in the JS file

--- app/components/people-list.js
+++ app/components/people-list.js
@@ -1,4 +1,12 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  actions: {
+    showPerson(person) {
+      alert(person);
+    },
+    talkToMe(person) {
+      alert(`Yo, my name is ${person}`);
+    }
+  }
 });
